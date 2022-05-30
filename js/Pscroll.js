9805396e5b350cafce7d8666c18dc30b507e8bcd
Baek MinKyung <en0008@naver.com>
2022-05-30 19:57:58 +0900

    2.0

---
+++ js/Pscroll.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".pf .center").on('mousewheel',function(e){
+        var wheelDelta = e.originalEvent.wheelDelta;
+        if(wheelDelta > 0){
+        //console.log("up");
+        $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+        }else{
+        //console.log("down");
+        $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
+        }
+    });
+});
