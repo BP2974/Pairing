1b59ad8cbb854dd8e0c985803df874385b73e0cd
Baek MinKyung <en0008@naver.com>
2022-06-07 02:52:39 +0900

    3.4

---
+++ js/click.js
@@ -0,0 +1,17 @@
+var P1 = document.getElementById("p1");
+var P2 = document.getElementById("p2");
+var P3 = document.getElementById("p3");
+var P4 = document.getElementById("p4");
+
+P1.onclick = function(){
+    P1.style.opacity = "0";
+}
+P2.onclick = function(){
+    P2.style.opacity = "0";
+}
+P3.onclick = function(){
+    P3.style.opacity = "0";
+}
+P4.onclick = function(){
+    P4.style.opacity = "0";
+}
